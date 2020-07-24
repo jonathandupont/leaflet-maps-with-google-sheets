a5c720f18fd1431205edcf3cee70feb0eada742e
jonathandupont <jonathandupont@gmail.com>
2020-07-24 15:04:08 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1x9gPGcabhin4LbqXrSr4BAVh3fIr0BUNB8z6VfK9qEs/edit#gid=0';
